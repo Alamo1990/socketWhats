8c45401c928298330fed0527df30e4b79b245ac9
AndresSC <aaasssccc@gmail.com>
2017-03-20 10:41:44 +0100

    headers

--- server.c
+++ server.c
@@ -1,3 +1,15 @@
-
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "queue.h"
+
+
+int main(int argc, char**argv){
+
+
+
+ return 0;
+}
